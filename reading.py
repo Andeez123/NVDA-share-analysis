924d55d2bb0d515c157ffaa6df5ce1d3d9ec6d06
Andrew <117618414+Andeez123@users.noreply.github.com>
2025-03-10 19:30:18 +0800

    plot the data on a 5 year trend

--- reading.py
+++ reading.py
@@ -1,6 +1,10 @@
 from sqlalchemy import create_engine, text
 import pandas as pd
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+plt.style.use('seaborn')
+mpl.rc('axes', titlesize=14, titleweight='semibold')
 
 user = 'root'
 password = '1234'
@@ -27,10 +31,24 @@ print('Data fetched succesfully')
 
 df = pd.DataFrame(rows, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'], index=None)
 
-df['Returns'] = df['Close'].pct_change()
+df['Returns'] = df['Close'].pct_change() #calculate returns based on percentage change from the previous day
+df['Date'] = pd.to_datetime(df['Date'], format='%b %d, %Y')
 
-plt.figure(figsize=(16, 10))
-plt.hist(df['Returns'], density=True, bins = 200)
+#obtaining 5 year data
+df_split = df[df['Date'].between('2020-01-01', '2025-03-06')].iloc[::-1]
+trading_days = df_split['Date']
+
+fig, ax = plt.subplots(figsize=(16,10))
+ax.plot(trading_days, df_split['Close'],label="Closing Price", color="#3498db")
+ax.set_title("Nvidia Stock Prices with Indicators", fontsize="14", fontweight="semibold")
+ax.set_xlim([trading_days.min(), trading_days.max()])
+
+#format the plots as yearly data
+ax = plt.gca()  # Get current axis
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))  # Format as year
+ax.xaxis.set_major_locator(mdates.YearLocator(base = 1))
+
+plt.tight_layout()
 plt.show()
 
 
