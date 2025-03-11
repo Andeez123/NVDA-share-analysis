58799791dc2deaac25e140105abe09140b39dd43
Andrew <117618414+Andeez123@users.noreply.github.com>
2025-03-12 01:47:09 +0800

    time series plot of closing prices and attempt on volatility analysis

--- reading.py
+++ reading.py
@@ -29,26 +29,46 @@ with engine.connect() as connection:
 
 print('Data fetched succesfully')
 
+# date: trading date, open: opening price of stock for the day
+# high: highest price point of the stock, low: lowest price point for the day
+#close: closing price of the stock, adj close: adjusted closing price, factoring in corporate actions
+#volume: total trading volume of the stock
 df = pd.DataFrame(rows, columns=['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'], index=None)
 
 df['Returns'] = df['Close'].pct_change() #calculate returns based on percentage change from the previous day
-df['Date'] = pd.to_datetime(df['Date'], format='%b %d, %Y')
+df['Date'] = pd.to_datetime(df['Date'], format='%b %d, %Y') #convert date column to datetime format
+df.set_index('Date', inplace=True)
+# Specific goals on this dataset:
+# Trend Analysis: Identifying long-term trends in stock prices and market movements.
+# Volatility Assessment: Evaluating the stability and risk associated with different stocks based on their price fluctuations.
+# Correlation Study: Investigating how different stocks correlate with each other, understanding market segments and diversification opportunities.
+# Risk-Return Trade-off Analysis: Analyzing the balance between the potential risks and rewards of different stocks, aiding in portfolio management.
 
+#Time series of 5 years of closing prices:
 #obtaining 5 year data
-df_split = df[df['Date'].between('2020-01-01', '2025-03-06')].iloc[::-1]
-trading_days = df_split['Date']
+# df_split = df[df['Date'].between('2020-01-01', '2025-03-06')].iloc[::-1]
+# trading_days = df_split['Date']
 
-fig, ax = plt.subplots(figsize=(16,10))
-ax.plot(trading_days, df_split['Close'],label="Closing Price", color="#3498db")
-ax.set_title("Nvidia Stock Prices with Indicators", fontsize="14", fontweight="semibold")
-ax.set_xlim([trading_days.min(), trading_days.max()])
+# fig, ax = plt.subplots(figsize=(16,10))
+# ax.plot(trading_days, df_split['Close'],label="Closing Price", color="#3498db")
+# ax.set_title("Nvidia Stock Prices with Indicators", fontsize="14", fontweight="semibold")
+# ax.set_xlim([trading_days.min(), trading_days.max()])
 
-#format the plots as yearly data
-ax = plt.gca()  # Get current axis
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))  # Format as year
-ax.xaxis.set_major_locator(mdates.YearLocator(base = 1))
+# ax.plot(trading_days, df_split['Open'], label = 'Returns')
 
-plt.tight_layout()
-plt.show()
+# #format the plots as yearly data
+# ax = plt.gca()  # Get current axis
+# ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))  # Format as year
+# ax.xaxis.set_major_locator(mdates.YearLocator(base = 1))
+
+# plt.tight_layout()
+# plt.show()
+
+#Volatility analysis:
+df = df.iloc[::-1]
+df['Volatility'] = df['Close'].rolling(window='7D').std()
+print(df['Volatility'])
+
+
 
 
